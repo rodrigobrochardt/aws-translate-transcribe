3a0fd47aa3f83cfce764fbeef4698f64a8e5b9ff
Rodrigo <rodrigo.brochardt@fastsolucoes.com.br>
2021-11-30 10:47:58 -0300

    ajustes

--- webserver.py
+++ webserver.py
@@ -12,19 +12,22 @@ cors = CORS(app)
 app.config["DEBUG"] = True
 app.config["CORS_HEADERS"] = 'Content-Type'
 
+aws_access_key_id="ASIAZ2PGSF5Q5MGTWRS5"
+aws_secret_access_key="t0hXvZldSzzlDBmUVqclArn7WxurokRUzMP/R5ES"
+aws_session_token="FwoGZXIvYXdzEDYaDD7qiMDBUym80nxITiK4AUPCZE0rOzvwSHzQBZns1PG2C37gSeldaL62FAbuRm1SRG6drfhca8XbwaaMrcrT1CzBJuvpnCM7u7oz5eYqC/sX8azuudM2fx9wL1VlWKup0GRMKYXIk5jhEarcEwNTojMM1EF9vBZVbzx1/33pXiWZeR/GFs7V23CyuD1o266A512u8DFfEFic5xyv82EOELt6fzWOPb+kbykFmNP7SJqtB09QOZZJpkw36GgbCtYCn3Yn9PgFGL8oycGYjQYyLSGojvRCfacODWajERJrOUu/49n6fA3jk0oQNPEf5yeLS1gZn75UUO2dDRqA9A=="
+
 s3_client = boto3.client("s3",region_name="us-east-1",
-                              aws_access_key_id="ASIAZ2PGSF5Q72L6WHWA",
-                              aws_secret_access_key="1pdjOc4ye2JpCVRKlKB0KRI2lJh092xvNvwrDs/r",
-                              aws_session_token="FwoGZXIvYXdzECUaDNAo3obzntbo++eNsiK4Ad8cpVgbtEzknlcpeHPPaV+OWGERKqRTjb0s8MFIn6gdZ1U50EuEyAWlrf8UqnRGxCLguUTpTD9PC80cekQuKY4uXlnrGfknJHqGoiPYw59gT4aqVfChqdNnVzu6Fi3iVKN4jG4rVWf6L3fSHtlCME6uu3UprDilGn5yQ7BM++fwrGdamqIWMagdWUt4CDN9QnKwXDNSbcvofwIXiSHKwpNPJrLNpBUFZc0y+YiYNNBBXfBP8bL26ncosM+UjQYyLW4mvNI6Nl8T7I1Ha828prA5wleoRMMCP2XYSaWF+NkPvtX6Y4goRFtX1KCxwg==")
+                              aws_access_key_id=aws_access_key_id,
+                              aws_secret_access_key=aws_secret_access_key,
+                              aws_session_token=aws_session_token)
 transcribe_client = boto3.client("transcribe",region_name="us-east-1",
-                                              aws_access_key_id="ASIAZ2PGSF5Q72L6WHWA",
-                              aws_secret_access_key="1pdjOc4ye2JpCVRKlKB0KRI2lJh092xvNvwrDs/r",
-                              aws_session_token="FwoGZXIvYXdzECUaDNAo3obzntbo++eNsiK4Ad8cpVgbtEzknlcpeHPPaV+OWGERKqRTjb0s8MFIn6gdZ1U50EuEyAWlrf8UqnRGxCLguUTpTD9PC80cekQuKY4uXlnrGfknJHqGoiPYw59gT4aqVfChqdNnVzu6Fi3iVKN4jG4rVWf6L3fSHtlCME6uu3UprDilGn5yQ7BM++fwrGdamqIWMagdWUt4CDN9QnKwXDNSbcvofwIXiSHKwpNPJrLNpBUFZc0y+YiYNNBBXfBP8bL26ncosM+UjQYyLW4mvNI6Nl8T7I1Ha828prA5wleoRMMCP2XYSaWF+NkPvtX6Y4goRFtX1KCxwg==")
-translate_client = boto3.client("translate",
-                                              region_name="us-east-1",
-                                              aws_access_key_id="ASIAZ2PGSF5Q72L6WHWA",
-                              aws_secret_access_key="1pdjOc4ye2JpCVRKlKB0KRI2lJh092xvNvwrDs/r",
-                              aws_session_token="FwoGZXIvYXdzECUaDNAo3obzntbo++eNsiK4Ad8cpVgbtEzknlcpeHPPaV+OWGERKqRTjb0s8MFIn6gdZ1U50EuEyAWlrf8UqnRGxCLguUTpTD9PC80cekQuKY4uXlnrGfknJHqGoiPYw59gT4aqVfChqdNnVzu6Fi3iVKN4jG4rVWf6L3fSHtlCME6uu3UprDilGn5yQ7BM++fwrGdamqIWMagdWUt4CDN9QnKwXDNSbcvofwIXiSHKwpNPJrLNpBUFZc0y+YiYNNBBXfBP8bL26ncosM+UjQYyLW4mvNI6Nl8T7I1Ha828prA5wleoRMMCP2XYSaWF+NkPvtX6Y4goRFtX1KCxwg==")
+                              aws_access_key_id=aws_access_key_id,
+                              aws_secret_access_key=aws_secret_access_key,
+                              aws_session_token=aws_session_token)
+translate_client = boto3.client("translate",region_name="us-east-1",
+                              aws_access_key_id=aws_access_key_id,
+                              aws_secret_access_key=aws_secret_access_key,
+                              aws_session_token=aws_session_token)
 def isFileExists():
   try:
     s3_client.get_object(Bucket='bucket-projeto-pisi4', Key='audio.mp3')
@@ -75,7 +78,6 @@ def transcribe():
 @cross_origin()
 def translate():
   text = request.data.decode()[6:].rstrip()
-  print(text)
   textTranslated = translate_client.translate_text(Text=text,SourceLanguageCode="auto",TargetLanguageCode="en")["TranslatedText"]
   return flask.redirect("http://127.0.0.1:5500/?translated={}".format(textTranslated))
 app.run()
